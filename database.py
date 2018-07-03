55a204bb23be018d292890c89be0866f42b48737
SCys <me@iscys.com>
2018-07-03 22:25:40 +0800

    * updated

--- database.py
+++ database.py
@@ -21,22 +21,23 @@ def gen_async(*args, **kwargs) -> Engine:
     global _db
 
     if _db is None:
-
         registry.register('postgresql.asyncpg', 'gino.dialects.asyncpg', 'AsyncpgDialect')
         registry.register('asyncpg', 'gino.dialects.asyncpg', 'AsyncpgDialect')
 
         kwargs.setdefault('strategy', 'gino')
-        _db = asyncio.get_event_loop().run_until_complete(gen_sync(*args, **kwargs))
+        kwargs.setdefault('json_serializer', lambda obj: json.dumps(obj, datetime_mode=json.DM_ISO8601))
+        kwargs.setdefault('json_deserializer', lambda obj: json.load(obj, datetime_mode=json.DM_ISO8601))
+        _db = asyncio.get_event_loop().run_until_complete(create_engine(options.db, *args, **kwargs))
         md.bind = _db
 
     return _db
 
 
 def gen_sync(*args, **kwargs) -> Engine:
+    kwargs.setdefault('pool_size', 30)
     kwargs.setdefault('pool_timeout', 30)
+    kwargs.setdefault('max_overflow', 50)
     kwargs.setdefault('json_serializer', lambda obj: json.dumps(obj, datetime_mode=json.DM_ISO8601))
     kwargs.setdefault('json_deserializer', lambda obj: json.load(obj, datetime_mode=json.DM_ISO8601))
-    kwargs.setdefault('pool_size', 30)
-    kwargs.setdefault('max_overflow', 50)
 
     return create_engine(options.db, *args, **kwargs)
